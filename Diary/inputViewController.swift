1ce9c9f96f019b5d01e86f5bd479b9fad21324ed
及川雄太 <oikenfight@gmail.com>
2016-01-06 23:52:20 +0900

    create segue

---
+++ Diary/inputViewController.swift
@@ -0,0 +1,35 @@
+//
+//  inputViewController.swift
+//  Diary
+//
+//  Created by 及川雄太 on 2016/01/06.
+//  Copyright © 2016年 及川雄太. All rights reserved.
+//
+
+import UIKit
+
+class inputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
